d552309b12709d944c64a0c47b29915a4ec461b5
Michael Zorin <michael.zorin.rd@gmail.com>
2023-11-08 21:47:54 +0100

    intermediate commit

--- modules/api.js
+++ modules/api.js
@@ -1,31 +1,63 @@
 //  API operations
 //
 //import { getCommentsAPI } from './main.js';
+import { initiateLikeButtonListeners, initiateReplyListeners, initiateEditSaveListeners, validationButton, inputByEnter } from "./features.js";
+import { getNetworkDate } from "./getDate.js";
 
 const nameInputElement = document.getElementById("comment-name-input");
 const textInputElement = document.getElementById("comment-text-input");
 const buttonElement = document.getElementById("comment-button");
 
+const commentWaitElement = document.getElementById("comment-wait");
+const commentUploadElement = document.getElementById("comment-upload");
+
 const errorShortInputElement = document.getElementById("error-short-input");
 const errorNoNetworkElement = document.getElementById("error-no-network");
 const errorServerDownElement = document.getElementById("error-server-down");
 
-export let token;
-export const setToken = (newToken) => {
-    token = newToken;
-}
-
 const commentsURL = "https://wedev-api.sky.pro/api/v2/oso4/comments";
 const userURL = "https://wedev-api.sky.pro/api/user/login";
+
+let commentsArray = [];
+
+export let token;
+console.log(token);
+export const setUserToken = (newToken) => {
+    token = newToken;
+};
+
+export let name;
+console.log(name);
+export const setUserName = (newName) => {
+    name = newName;
+};
+
+export let id;
+console.log(id);
+export function setUserId(newId) {
+    id = newId;
+};
 
 const login = (userLogin, userPassword) => {
     return fetch(userURL, {
         method: "POST",
         body: JSON.stringify(userLogin, userPassword),
-    }).then((response) => response.json());
+    }).then((responseData) => {
+        if (responseData.status === 201) {
+            return responseData.json();
+        } else if (responseData.status === 400) {
+            throw new Error("Incorrect login or password");
+        }
+    }).catch((error) => {
+        if (error.message === "Incorrect login or password") {
+            console.log('Incorrect login or password');
+            alert('Incorrect login or password!')
+        }
+    })
 }
 
-const getCommentsAPICore = () => {
+// fetch GET from API
+export const getComments = () => {
     return fetch(commentsURL, {
         method: "GET",
         headers: {
@@ -33,12 +65,34 @@ const getCommentsAPICore = () => {
         },
     })
         .then((response) => {
-            if (response.status === 401) {
-                throw new Error("No authorization");
-            }
-            return response.json()
-        });
-}
+            return response.json();
+        })
+        .then((response) => {
+            commentWaitElement.style.display = "none";
+            return response;
+        })
+        .then((responseData) => {
+
+
+            commentsArray = responseData.comments.map((comment) => {
+                return {
+                    author: comment.author.name,
+                    date: getNetworkDate(comment.date),
+                    text: comment.text,
+                    likes: comment.likes,
+                    isLiked: false,
+                    isEdit: false,
+
+                    id: comment.id,
+                };
+            });
+
+            renderComments();
+
+            console.log(commentsArray);
+        })
+};
+
 const sanitizeInput = (input) => input.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
 
 const retryPostComment = () => {
@@ -123,4 +177,254 @@ const retryPostComment = () => {
     };
 };
 
+// add to array and render function
+const renderComments = () => {
+    const appElement = document.querySelector("#app");
+    const сommentsArrayHTML = commentsArray
+        .map((comment, index) => {
+
+            const quoteText = comment.text.replace(/&gt; /g, '<div class="quote">').replace(/, /g, '</div><br>');
+
+            const editButtonHtml = comment.isEdit
+                ? `<button data-index='${index}' type='button' class='save-btn'>Сохранить</button>`
+                : `<button data-index='${index}' type='button' class='edit-btn'>Редактировать</button>`;
+
+            const commentTextHtml = comment.isEdit
+                ? `<textarea data-index='${index}' id="textarea-${index}" class="edit-textarea">${comment.text}</textarea>`
+                : `<div class="comment-text">${quoteText}</div>`;
+
+            return `
+                <li class="comment" data-delete="${index}">
+                    <div class="comment-header">
+                        <div>${comment.author}</div>
+                        <div>${comment.date}</div>
+                    </div>
+
+                    <div class="comment-body">
+                        ${commentTextHtml}
+                    </div>
+
+                    <div class="comment-footer">
+                        <div class="likes">
+                            <span class="likes-counter">${comment.likes}</span>
+                            <button data-index="${index}" class="like-button ${comment.isLiked ? '-active-like' : ''}"></button>
+                    </div>
+                
+                    ${editButtonHtml}
+                </li>`;
+        }).join("");
+
+
+    const appHTML = `
+        <!--<div id="load" class="text-load">
+            <p>Обновление списка комментариев. Пожалуйста подождите.</p>
+        </div>-->
+
+        <ul class="comments">
+            ${сommentsArrayHTML}
+        </ul>
+
+        <br>
+
+        <div style="display: none; color: rgb(255, 255, 255)" class="loading-add">
+            <p>Комментарий добавляется...</p>
+        </div>
+
+        <div id="add-form" class="add-form">
+            <input value="${name}" readonly id="comment-name-input" type="text" class="add-form-name" placeholder="Введите ваше имя" />
+            <textarea
+                type="textarea"
+                class="add-form-text"
+                id="comment-text-input"
+                placeholder="Введите ваш коментарий"
+                rows="4">
+            </textarea>
+
+            <div class="add-form-row">
+                <button class="add-form-button-active" id="comment-button">Написать</button>
+            </div>
+            
+        </div>`;
+    appElement.innerHTML = appHTML;
+
+
+    initiateLikeButtonListeners();
+    initiateReplyListeners();
+    initiateEditSaveListeners();
+
+    inputByEnter();
+
+    //validationButton();
+
+    const nameInputElement = document.getElementById("comment-name-input");
+    const textInputElement = document.getElementById("comment-text-input");
+    //const inputName = document.getElementById("comment-name-input");
+    //const inputComments = document.getElementById("comment-text-input");
+    const button = document.getElementById("comment-button");
+
+    button.addEventListener("click", () => {
+        console.log(1);
+        nameInputElement.classList.remove("error");
+        if (nameInputElement.value === "") {
+            nameInputElement.classList.add("error");
+        }
+        textInputElement.classList.remove("error");
+        if (textInputElement.value === "") {
+            textInputElement.classList.add("error");
+            return;
+        }
+
+        retryPostComment();
+
+    });
+
+    //inputComments.addEventListener("input", validationButton);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  OLD
+const getCommentsAPICore = () => {
+    return fetch(commentsURL, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+        .then((response) => {
+            if (response.status === 401) {
+                throw new Error("No authorization");
+            }
+            return response.json()
+        });
+}
+
 export { getCommentsAPICore, retryPostComment, login }
+
+/*
+// sample function for retry/post comments
+const addComment = (name, text) => {
+    return fetch(commentsURL, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            text: text
+                .replaceAll("&", "&amp;")
+                .replaceAll("<", "&lt;")
+                .replaceAll(">", "&gt;")
+                .replaceAll('"', "&quot;"),
+            name: name
+                .replaceAll("&", "&amp;")
+                .replaceAll("<", "&lt;")
+                .replaceAll(">", "&gt;")
+                .replaceAll('"', "&quot;"),
+            forceError: true,
+        }),
+    });
+};
+
+// Метод fetch() запрос через API на добавление данных c сохранением на сервере комментарий в списке
+const sedingsServer = () => {
+    const formElements = document.getElementById("add-form");
+    const inputName = document.getElementById("comment-name-input");
+    const inputComments = document.getElementById("comment-text-input");
+    const loadingElements = document.querySelector(".loading-add");
+    loadingElements.style.display = "block";
+    formElements.style.display = "none";
+
+    // Функция addComment отдельно для передачи POST запроса
+    addComment(inputName.value, inputComments.value)
+        .then((response) => {
+            return response;
+        })
+        // Создан отдельный then для реализации логики if/else и передачи исключений методом throw new Error() отлавливаем ошибки в catch()
+        .then((responseData) => {
+            if (responseData.status === 201) {
+                return responseData.json();
+            } else if (responseData.status === 400) {
+                throw new Error("Не верный ввод");
+            } else if (responseData.status === 500) {
+                console.log(responseData.status);
+                throw new Error("Сломался сервер");
+            } else {
+                throw new Error("Не работает интернет");
+            }
+        })
+        .then(() => {
+            return getComments(); // Вызов повторно метод GET в методе POST для того что бы добавлялся комментарий
+        })
+        .then(() => {
+            button.disabled = true;
+            inputName.value = "";
+            inputComments.value = "";
+            loadingElements.style.display = "none";
+            formElements.style.display = "flex";
+        })
+        .catch((error) => {
+            loadingElements.style.display = "none";
+            formElements.style.display = "flex";
+            if (error.message === "Не верный ввод") {
+                alert("Имя и комментарий должны быть не короче 3 символов");
+            } else if (error.message === "Сломался сервер") {
+                addCommentError();
+            }
+        });
+};
+
+// Функция повторного отправления запроса при 500 ошибки API
+function addCommentError() {
+    addComment(inputName.value, inputComments.value)
+        .then((response) => {
+            return response;
+        }) // Создан отдельный then для реализации логики if/else и передачи исключений методом throw new Error() отлавливаем ошибки в catch()
+        .then((responseData) => {
+            console.log(responseData);
+            if (responseData.status === 500) {
+                throw new Error("Сломался сервер");
+            } else {
+                return responseData.json();
+            }
+        })
+        .then(() => {
+            return getComments();
+        })
+        .then(() => {
+            button.disabled = true;
+            inputName.value = "";
+            inputComments.value = "";
+            loadingElements.style.display = "none";
+            formElements.style.display = "flex";
+        })
+        .catch((error) => {
+            alert(
+                "Кажется, что на сервере проблемы делаем повторный запрос!!!!!!!!!"
+            );
+            loadingElements.style.display = "block";
+            formElements.style.display = "none";
+            addCommentError();
+        });
+}
+*/
--- modules/main.js
+++ modules/main.js
@@ -1,61 +1,297 @@
 "use strict";
 
 import { getNetworkDate } from './getDate.js';
-import { getCommentsAPICore, retryPostComment } from './api.js';
+import { login, setUserToken, setUserName, setUserId, getComments } from './api.js';
+/*import { getCommentsAPICore, retryPostComment } from './api.js';
 import { renderComments } from './renderComments.js';
 import { validationFields } from './validationFields.js';
 import { loadingText } from './loadingText.js';
-import { renderLogin } from "./renderLogin.js";
+import { renderLogin } from "./renderLogin.js";*/
 
 const nameInputElement = document.getElementById("comment-name-input");
 const textInputElement = document.getElementById("comment-text-input");
 const buttonElement = document.getElementById("comment-button");
 
+const commentWaitElement = document.getElementById("comment-wait");
+commentWaitElement.style.display = "block";
+
+
+
+export const now = new Date();
+const URL = "https://wedev-api.sky.pro/api/v2/oso4/comments";
+
+export const loadPage = () => {
+    return fetch(URL, {
+        method: "GET",
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            commentWaitElement.style.display = "none";
+            return response;
+        })
+        .then((responseData) => {
+            const comments = responseData.comments.map((comment) => {
+                return {
+                    author: comment.author.name,
+                    date: getNetworkDate(comment.date),
+                    text: comment.text,
+                    likes: comment.likes,
+                    isLiked: false,
+                    isEdit: false,
+                };
+            });
+
+            console.log(comments);
+            renderComments(comments)
+
+        })
+        .catch((error) => {
+            //alert("Кажется, у вас сломался интернет");
+            console.error(error);
+        });
+};
+loadPage();
+
+const renderComments = (commentsArray) => {
+    const appElement = document.querySelector("#app");
+    const commentsArrayHTML = commentsArray
+        .map((comment, index) => {
+
+            const quoteText = comment.text.replace(/&gt; /g, '<div class="quote">').replace(/, /g, '</div><br>');
+
+            const editButtonHtml = comment.isEdit
+                ? `<button data-index='${index}' type='button' class='save-btn'>Сохранить</button>`
+                : `<button data-index='${index}' type='button' class='edit-btn'>Редактировать</button>`;
+
+            const commentTextHtml = comment.isEdit
+                ? `<textarea data-index='${index}' id="textarea-${index}" class="edit-textarea">${comment.text}</textarea>`
+                : `<div class="comment-text">${quoteText}</div>`;
+
+            return `
+                <li class="comment" data-delete="${index}">
+                    <div class="comment-header">
+                        <div>${comment.author}</div>
+                        <div>${comment.date}</div>
+                    </div>
+
+                    <div class="comment-body">
+                        ${commentTextHtml}
+                    </div>
+
+                    <div class="comment-footer">
+                        <div class="likes">
+                            <span class="likes-counter">${comment.likes}</span>
+                            <button data-index="${index}" class="like-button ${comment.isLiked ? '-active-like' : ''}"></button>
+                    </div>
+                
+                    ${editButtonHtml}
+                </li>`;
+        }).join("");
+
+    const appHTML = `  
+    <ul class="comments">
+        ${commentsArrayHTML}
+    </ul>
+    
+    <br>
+    
+    <div>
+        <a id="link" href="#">Авторизуйтесь</a>, чтобы оставить комментарий.
+    </div>`;
+
+    appElement.innerHTML = appHTML;
+
+    initiateLikeButtonListeners(commentsArray);
+    initiateReplyListeners(commentsArray);
+    initiateEditSaveListeners(commentsArray);
+
+    document.getElementById('link').addEventListener('click', () => {
+        renderLogin()
+    })
+}
+
+function renderLogin() {
+    const appElement = document.querySelector("#app");
+    const loginHTML = `
+    <div class="container">
+        <div class="add-form">
+
+            <h3>Войти / Зарегистрироваться</h3>
+            <h4>osetr</h4>
+            <h4>abirvalg</h4>
+
+            <input
+                id="login-input"
+                type="text"
+                class="add-form-name"
+                placeholder="Введите ваш логин"
+            />
+            <br>
+            <input
+                id="login-password"
+                type="text"
+                class="add-form-name"
+                placeholder="Введите ваш пароль"
+            />
+
+            <div class="add-form-row">
+                <button id="login-button" class="add-form-button">Войти</button>
+            </div>
+
+        </div>
+        
+        <br>
+        
+        <div>
+            <a id="linkBack" href="#">Вернуться</a> к комментариям</div>
+        </div>
+    </div>`;
+
+    appElement.innerHTML = loginHTML;
+    //console.log(appElement.innerHTML);
+
+    document.querySelector("#linkBack").addEventListener("click", () => {
+        loadPage()
+    })
+    const loginInputElement = document.querySelector("#login-input");
+    const loginPasswordElement = document.querySelector("#login-password");
+    const loginButtonElement = document.querySelector("#login-button");
+
+    loginButtonElement.addEventListener("click", loginButton);
+
+    function loginButton() {
+        login({
+            login: loginInputElement.value,
+            password: loginPasswordElement.value,
+        })
+            .then((responseData) => {
+                setUserToken(responseData.user.token);
+                console.log(responseData.user.token);
+                setUserName(responseData.user.name);
+                console.log(responseData.user.name);
+                setUserId(responseData.user._id)
+
+            })
+            .then(() => {
+                getComments();
+            })
+    };
+};
+
+const initiateLikeButtonListeners = (commentsArray) => {
+    const likeButtonElements = document.querySelectorAll(".like-button");
+
+    for (const likeButtonElement of likeButtonElements) {
+        likeButtonElement.addEventListener("click", (event) => {
+            event.stopPropagation();
+
+            const index = likeButtonElement.dataset.index;
+
+            if (commentsArray[index].isLiked) {
+                //console.log('element unliked');
+                commentsArray[index].likes--;
+                commentsArray[index].isLiked = false;
+            } else {
+                //console.log('element liked');
+                commentsArray[index].likes++;
+                commentsArray[index].isLiked = true;
+            }
+
+            renderComments(commentsArray);
+        });
+    }
+};
+
+const initiateReplyListeners = (commentsArray) => {
+    const commentBoxElements = document.querySelectorAll(".comment");
+
+    for (const commentBoxElement of commentBoxElements) {
+        commentBoxElement.addEventListener("click", () => {
+            const index = commentBoxElement.dataset.index;
+            textInputElement.value = `> ${commentsArray[index].text} \n ${commentsArray[index].author}, `;
+            //console.log('commentBoxElement clicked');
+        })
+    };
+};
+
+const initiateEditSaveListeners = (commentsArray) => {
+    const buttons = document.querySelectorAll(".edit-btn, .save-btn");
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", (event) => {
+            event.stopPropagation();
+            if (button.classList.contains("edit-btn")) {
+                commentsArray[index].isEdit = true;
+                //console.log('comment editing');
+            } else if (button.classList.contains("save-btn")) {
+                const index = button.dataset.index;
+                const textarea = document.getElementById(`textarea-${index}`);
+                commentsArray[index].text = textarea.value;
+                commentsArray[index].isEdit = false;
+                //console.log('comment saved');
+            }
+            renderComments(commentsArray);
+        });
+    });
+};
+
+
+
+
+
+
+
+// OLD
+/*
 let comments = [];
 
-export const getCommentsAPI = () => {
-    return getCommentsAPICore().then((responseData) => {
-        comments = responseData.comments.map((comment) => {
-            return {
-                author: comment.author.name,
-                date: getNetworkDate(comment.date),
-                likes: comment.likes,
-                isLiked: false,
-                text: comment.text,
-            };
+    export const getCommentsAPI = () => {
+        return getCommentsAPICore().then((responseData) => {
+            comments = responseData.comments.map((comment) => {
+                return {
+                    author: comment.author.name,
+                    date: getNetworkDate(comment.date),
+                    likes: comment.likes,
+                    isLiked: false,
+                    text: comment.text,
+                };
+            });
+            renderComments(comments);
         });
-        renderComments(comments);
+    }
+    
+    const loadingText = () => {
+        document.getElementById("comments").style.display = 'none';
+        document.getElementById("comment-wait").style.display = 'block';
+        getCommentsAPI()
+            .then(() => {
+                document.getElementById("comments").style.display = 'flex';
+                document.getElementById("comment-wait").style.display = 'none';
+            });
+    };
+
+
+    // later add loadingText once again, now it's switched off and replaced with
+    // simple loading comments
+
+    loadingText();
+    //getCommentsAPI();
+    renderLogin();
+
+
+    
+    validationFields();
+    nameInputElement.classList.remove("error");
+    textInputElement.classList.remove("error");
+    nameInputElement.addEventListener("input", validationFields);
+    textInputElement.addEventListener("input", validationFields);
+    
+
+    buttonElement.addEventListener("click", () => {
+        buttonElement.disabled = true;
+        buttonElement.textContent = 'Добавление...';
+        retryPostComment();
     });
-}
-/*
-const loadingText = () => {
-    document.getElementById("comments").style.display = 'none';
-    document.getElementById("comment-wait").style.display = 'block';
-    getCommentsAPI()
-        .then(() => {
-            document.getElementById("comments").style.display = 'flex';
-            document.getElementById("comment-wait").style.display = 'none';
-        });
-};*/
-
-
-// later add loadingText once again, now it's switched off and replaced with
-// simple loading comments
-//loadingText();
-getCommentsAPI();
-renderLogin();
-
-
-/*
-validationFields();
-nameInputElement.classList.remove("error");
-textInputElement.classList.remove("error");
-nameInputElement.addEventListener("input", validationFields);
-textInputElement.addEventListener("input", validationFields);
-*/
-
-buttonElement.addEventListener("click", () => {
-    buttonElement.disabled = true;
-    buttonElement.textContent = 'Добавление...';
-    retryPostComment();
-});
+    */
